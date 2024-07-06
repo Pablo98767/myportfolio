78392133ba0224a47d83c6c786782a7f96c5921f
Pablo <pablofelix678c@gmail.com>
2024-07-06 12:09:22 -0300

    fazendo dedeploy das alterações das seções, colocando meus  propios textos e ajustando cores do botão de "Entre em contato" de verde paraa azul.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React from "react";
-import React from "react";
 import About from "./components/About";
 import Contact from "./components/contact";
 import Navbar from "./components/navbar";
 import Projects from "./components/projects";
 import Skills from "./components/skills";
 import Testimonials from "./components/testimonials";
-
+import Login from "./components/login";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 
 export default function App() {
   return (
     <main className="text-gray-400 bg-gray-900 body-font">
-     <Navbar />
-     <About />
-     <Projects />
-     <Skills />
-     <Testimonials />
-     <Contact />
-   </main>
+      <Navbar />
+      <About />
+      <Projects />
+      <Skills />
+      <Testimonials />
+      <Contact />
+  
+    </main>
+    
+   
      
   );
 }
--- src/components/About.js
+++ src/components/About.js
@@ -6,25 +6,22 @@ export default function About() {
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-            Hi, I'm Reed.
-            <br className="hidden lg:inline-block" />I love to build amazing
-            apps.
+            Oi, Eu me chamo Pablo
+            <br className="hidden lg:inline-block" /> e sou analista de dados e sistemas Junior.
           </h1>
           <p className="mb-8 leading-relaxed">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
-            laborum quasi, incidunt dolore iste nostrum cupiditate voluptas?
-            Laborum, voluptas natus?
+           Meu objetivo é compreender e resolver seus problemas usando técnica de análise de dados, inteligência artificial.
           </p>
           <div className="flex justify-center">
             <a
               href="#contact"
-              className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
-              Work With Me
+              className="inline-flex text-white bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-blue-600 rounded text-lg">
+              Entre em contato
             </a>
             <a
               href="#projects"
               className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
-              See My Past Work
+             Veja meu portfólio
             </a>
           </div>
         </div>
